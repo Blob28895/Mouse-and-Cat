97eeb4e9ba9f628ad729c1286a1c351945bd1b7d
Blob28895 <blob28895@gmail.com>
2023-07-08 14:03:31 -0500

    updated platform script to only fall through a platform when pressing S AND standing on the platform

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -7,6 +7,7 @@ public class Platform : MonoBehaviour
 {
 
     private PlatformEffector2D _effector;
+	private bool _touchingPlayer = false;
 	[SerializeField] private InputReaderSO _inputReader = default;
 	// Start is called before the first frame update
 	void Start()
@@ -32,14 +33,38 @@ public class Platform : MonoBehaviour
         {
             _effector.rotationalOffset = 0;
         }
-        else if(Input.GetAxisRaw("Vertical") < 0)
+        else if(Input.GetAxisRaw("Vertical") < 0 && _touchingPlayer)
         {
             _effector.rotationalOffset = 180;
-        }
+			//Invoke(nameof(testerFunction), 0.5f);
+		}
     }
 
     private void jumpSetOffset(InputAction.CallbackContext context)
     {
         _effector.rotationalOffset = 0;
+	}
+
+    private void testerFunction()
+    {
+        _effector.rotationalOffset = 0;
     }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			//Debug.Log("Touched by Player");
+			_touchingPlayer = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			//Debug.Log("Player Left me");
+			_touchingPlayer = false;
+		}
+	}
 }
