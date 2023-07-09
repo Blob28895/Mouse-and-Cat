573c3d65d495c5fa1e71ebaa6b905e12705e7990
Blob28895 <blob28895@gmail.com>
2023-07-08 22:54:13 -0500

    added main menu

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+	
+	public void loadNextScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void quitGame()
+	{
+		Application.Quit();
+	}
+}
