3ca855a1716f451a997ddf7c7133d172576e6838
Blob28895 <blob28895@gmail.com>
2023-07-26 22:58:13 -0500

    added event system to kitchen, added level select, fixed main menu button on game over screen

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+	[Tooltip("Canvas items that will only appear when not selecting a level")]
+	[SerializeField] private GameObject mainMenuObjects;
+
+	[Tooltip("Canvas items that will only appear when selecting a level")]
+	[SerializeField] private GameObject levelSelectObjects;
 	public void loadNextScene()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,5 +23,22 @@ public class MenuButtons : MonoBehaviour
 	public void loadPreviousScene()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	}
+
+	public void showLevelSelect()
+	{
+		mainMenuObjects.SetActive(false);
+		levelSelectObjects.SetActive(true);
+	}
+
+	public void hideLevelSelect()
+	{
+		levelSelectObjects.SetActive(false);
+		mainMenuObjects.SetActive(true);
+	}
+
+	public void loadSceneByName(string name)
+	{
+		SceneManager.LoadScene(name);
 	}
 }
